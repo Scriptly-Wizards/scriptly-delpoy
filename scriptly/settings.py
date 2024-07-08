84569f682a2f81be8e1db9481eb7950d23d4aa18
Mario <liang.chen829@gmail.com>
2024-07-07 18:34:00 -0700

    chore: Add 'scriply-lena-zhangs-projects.vercel.app' to ALLOWED_HOSTS

--- scriptly/settings.py
+++ scriptly/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-@8guaz85_8xl3uw&wf359no19tjom&_l93167(!jws#9te8d^#
 DEBUG = True
 
 ALLOWED_HOSTS = ['scriptly-105d4cc5ed46.herokuapp.com',
-                 'localhost', '127.0.0.1', '0.0.0.0']
+                 'localhost', '127.0.0.1', '0.0.0.0', 'scriply-lena-zhangs-projects.vercel.app']
 
 
 # Application definition
