6a637400f0d8e446e771db50d35f5918d619f57e
Mario <liang.chen829@gmail.com>
2024-07-08 14:56:46 -0700

    chore: Update ALLOWED_HOSTS and CORS settings to include 'scriply-lena-zhangs-projects.vercel.app' and 'scriptly-wizard-7c14c9048571.herokuapp.com'

--- scriptly/settings.py
+++ scriptly/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-@8guaz85_8xl3uw&wf359no19tjom&_l93167(!jws#9te8d^#
 DEBUG = True
 
 ALLOWED_HOSTS = ['scriptly-105d4cc5ed46.herokuapp.com',
-                 'localhost', '127.0.0.1', '0.0.0.0']
+                 'localhost', '127.0.0.1', '0.0.0.0', 'scriptly-wizard-7c14c9048571.herokuapp.com']
 
 
 # Application definition
@@ -138,6 +138,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
     "https://scriply-lena-zhangs-projects.vercel.app",
 ]
 
